fa1795fac5eeafb3f6001bc9316b1f104386adc7
Shivam yadav <yadavshivam6925@gmail.com>
2025-02-21 13:53:04 +0530

    update UI

--- blogCard.js
+++ blogCard.js
@@ -1,22 +1,29 @@
 // Array of blog objects
 const blogs = [
   {
-      title: "Hosting a Web Page on GitHub Pages!",
-      description: "Learn how to host your website for free using GitHub Pages. This guide covers setting up a repository, uploading your HTML, CSS, and JavaScript files, and making your site live with a GitHub Pages URL. Perfect for developers, students, and project showcases! 🚀",
-      imgSrc: "https://www.spinxdigital.com/app/uploads/2024/03/Blog-Main-Image.jpg",
-      link: "https://example.com/github-pages-guide"
+    title: "Hosting a Web Page on GitHub Pages!",
+    description: "Learn how to host your website for free using GitHub Pages. This guide covers setting up a repository, uploading your HTML, CSS, and JavaScript files, and making your site live with a GitHub Pages URL. Perfect for developers, students, and project showcases! 🚀",
+    imgSrc: "https://www.spinxdigital.com/app/uploads/2024/03/Blog-Main-Image.jpg",
+    link: "https://example.com/github-pages-guide"
   },
   {
-      title: "Web Development Trends 2024",
-      description: "Stay updated with the latest trends in web development.",
-      imgSrc: "https://cdn-icons-png.flaticon.com/512/5969/5969113.png",
-      link: "https://example.com/web-dev-trends-2024"
+    title: "How to set up a Android Development Environment",
+    description: "Learn how to set up an Android development environment on your local machine. This guide covers installing Android Studio, setting up an emulator, and creating your first Android app. Perfect for beginners and students! 📱",
+    imgSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcxvx7-Wparxr1aKmUd9j2hH59oLWWX3LjPA&s",
+    link: "https://example.com/web-dev-trends-2024"
   },
   {
-      title: "CSS Animation Guide",
-      description: "Master CSS animations with this detailed guide.",
-      imgSrc: "https://cdn-icons-png.flaticon.com/512/5969/5969113.png",
-      link: "https://example.com/css-animation-guide"
+    title: "Learn how to set-up Android Emulator",
+    description: "Master the art of setting up an Android Emulator with this detailed guide.",
+    imgSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQQORjEE-W6ZhfBaNyRvnQJw54u_DILWGXCg&s",
+    link: "https://example.com/css-animation-guide"
+  },
+
+  {
+    title: "How to set up a React Development Environment",
+    description: "Learn how to set up a React development environment on your local machine. This guide covers installing Node.js, creating a new React app, and running your app in a development server. Perfect for beginners and students! ⚛️",
+    imgSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7oL9Lwj8hY09u2LF1xXYFIVSpXQilZXZZcw&s",
+    link: "https://example.com/web-dev-trends-2024"
   }
 ];
 
